cdc06a320d3c524bbd1bc2113d8b6ad54e34b863
Donie Kelly <donie.kelly@gmail.com>
2025-01-08 19:21:20 +0000

    Initial Commit

---
+++ PTSB_Demo/PTSB_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PTSB_DemoApp.swift
+//  PTSB_Demo
+//
+//  Created by Donie Kelly on 1/8/25.
+//
+
+import SwiftUI
+
+@main
+struct PTSB_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
