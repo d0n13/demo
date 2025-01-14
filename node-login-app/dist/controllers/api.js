781115a7dfc55c7aa8cfc7cb259588b475ee7725
Donie Kelly <donie.kelly@gmail.com>
2025-01-14 22:07:01 +0000

    removed dist folder

--- node-login-app/dist/controllers/api.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class API {
-    version(req, res) {
-        return res.status(200).json({ verison: '1.0.0' });
-    }
-}
-exports.default = new API();
--- node-login-app/dist/key/key.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.JWT_SECRET_KEY = void 0;
-exports.JWT_SECRET_KEY = 'my-secret-key';
